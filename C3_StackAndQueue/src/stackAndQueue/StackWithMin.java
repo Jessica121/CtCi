45b70bd243c7923e61f3e6a7e880521447742353
Jessica121 <janesis@hotmail.com>
2017-06-24 17:52:30 -0400

    3.2

--- C3_StackAndQueue/src/stackAndQueue/StackWithMin.java
+++ C3_StackAndQueue/src/stackAndQueue/StackWithMin.java
@@ -1,32 +1,32 @@
 package stackAndQueue;
 
-public class StackWithMin extends myStack<NodeWithMin>{
-	public void push(int data){	//overriding super classd myStack's push func cuz they have the same name;
-		int min = Math.min(data,this.min());
-		NodeWithMin add = new NodeWithMin(data,min);
-		super.push(add);
-	}
-	
-	public int min(){
-		if(this.isEmpty()){		//this-- superclass myStack's instance
-			return Integer.MAX_VALUE;
-		}
-		return peek().min;
-	}
-	
-	public static void main(String[] args){
-		StackWithMin stackWM = new StackWithMin();
-		stackWM.push(10);
-		stackWM.push(20);
-		stackWM.push(3);
-		stackWM.push(20);
-
-		System.out.println(stackWM.min());
-		System.out.println(stackWM.peek().value+" "+stackWM.peek().min);
-		System.out.println(stackWM.isEmpty());
-	}
-
-}
+//public class StackWithMin extends myStack<NodeWithMin>{
+//	public void push(int data){	//overriding super class myStack's push func cuz they have the same name;
+//		int min = Math.min(data,this.min());
+//		NodeWithMin add = new NodeWithMin(data,min);
+//		super.push(add);
+//	}
+//	
+//	public int min(){
+//		if(this.isEmpty()){		//this-- superclass myStack's instance
+//			return Integer.MAX_VALUE;
+//		}
+//		return peek().min;
+//	}
+//
+//	public static void main(String[] args){
+//		StackWithMin stackWM = new StackWithMin();
+//		stackWM.push(10);
+//		stackWM.push(20);
+//		stackWM.push(3);
+//		stackWM.push(20);
+//
+//		System.out.println(stackWM.min());
+//		System.out.println(stackWM.peek().value+" "+stackWM.peek().min);
+//		System.out.println(stackWM.isEmpty());
+//	}
+//	
+//}
 
 class NodeWithMin{
 	public int value;
@@ -35,4 +35,44 @@ class NodeWithMin{
 		this.value = v;
 		this.min = m;
 	}
+}
+
+//--------------------------------use another stack to track min, compare with it----------------------------------------
+public class StackWithMin extends myStack<Integer>{
+	myStack<Integer> minS;//ppt
+	public StackWithMin(){
+		minS = new myStack<Integer>();	//a stack to push min / pop 
+	}
+	public void push(int a){
+		if (a<=this.min()){
+			minS.push(a);
+		}
+		super.push(a);
+	}
+	
+	public Integer pop(){
+		int a = super.pop();
+		if(a == this.min()){
+			minS.pop();
+		}
+		return a;
+	}
+	
+	public int min(){
+		if(minS.isEmpty()){
+			return Integer.MAX_VALUE;
+		}else{
+			return minS.peek();
+		}
+	}
+	
+	public static void main(String[] args){
+	StackWithMin a = new StackWithMin();
+	a.push(2);
+	a.push(2);
+	a.push(1);
+	a.pop();
+	System.out.println(a.min());
+	}
+	
 }
