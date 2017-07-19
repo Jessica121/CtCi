231ed85888804cb78c24a9d0e319efab349b0a4e
Jessica121 <janesis@hotmail.com>
2017-07-18 20:27:36 -0400

    4.8 sol sblg

--- C4_TreesAndGraphs/src/findCommonAncestor.java
+++ C4_TreesAndGraphs/src/findCommonAncestor.java
@@ -1,7 +1,7 @@
 
 public class findCommonAncestor {
-	
-	public static TreeNode findCA(TreeNode a, TreeNode b){
+//	-----------------------------sol 1-------------------------------------
+	public static TreeNode findCA1(TreeNode a, TreeNode b){
 //		first trim the longer node's brance, make it align with the shorter one so they can go up together in one closure
 		int ha = getDpth(a);
 		int hb = getDpth(b);
@@ -37,14 +37,54 @@ public class findCommonAncestor {
 		return n;
 	}
 	
+//	-------------------------------sol 2-----------------------------------
+//	func a cover b, when a is b's parent or at least == b, so exclude not in the same branch
+//	sblg, if a & b is not each other's parent, get sbling, in diff branch , layer by layer's sibling , if other branch covers, 
+//	return the parent.
+//	has to have a non-null result for a and b in the same tree:)
+	public static boolean cover(TreeNode a, TreeNode b){
+//		cover b under a's leading branch
+		if(a == null || b == null){
+			return false;
+		}
+		if(a == b){
+			return true;
+		}
+		return cover(a.left , b) || cover(a.right , b);
+//		check all the nodes under a basically, multiple check on one child underneath it
+	}
+	
+	public static TreeNode getSblg(TreeNode a){
+		if(a == null || a.parent == null) return null;
+		TreeNode p = a.parent;
+		return a == p.left? p.right:p.left;
+	}
+	
+//	
+	public static TreeNode findCA2(TreeNode root, TreeNode a, TreeNode b){
+		if(!cover(root, a ) || !cover(root,b)) return null;
+//		a and b will in the graph.
+		if(cover(a,b)) return a;
+		if(cover(b,a)) return b;
+//		a and b will not in same branch
+		TreeNode sblg = getSblg(a);// doesnt matter get whose sblg, if b is higher, a's parent will go up too
+		TreeNode p = a.parent;
+		while(!cover(sblg, b)){
+//			get current sblg first, then 
+			sblg = getSblg(p);
+			p = p.parent;
+		}
+		return p;
+	}
+	
 	public static void main(String[] args) {
 		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		TreeNode root = TreeNode.createMinHeightTree(array);
-		TreeNode n3 = root.search(1);
+		TreeNode n3 = root.search(9);
 		TreeNode n7 = root.search(7);
-		TreeNode ancestor = findCA(n3, n7);
-		//System.out.println(ancestor.data);
-
+		TreeNode ancestor1 = findCA1(n3, n7);
+		TreeNode ancestor2 = findCA2(root,n3, n7);
+		System.out.println("Check me! "+ancestor1.data+" "+ancestor2.data);
 	}
 
 }
