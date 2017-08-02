8dc9a1dd8109324ca09c4b2c2de8e84221b12a1d
Jessica121 <janesis@hotmail.com>
2017-08-02 12:32:08 -0400

    4.10 better

--- C4_TreesAndGraphs/src/checkSubtree.java
+++ C4_TreesAndGraphs/src/checkSubtree.java
@@ -30,13 +30,38 @@ public class checkSubtree {
 		traverseAndAdd(n.right,s);
 //		O(n) time and O(n) space
 	}
+	
+	
+//	Sol traverse the big tree, iff the node's value equals the smalls tree's 'ROOT' value,
+//	check for every node together in two trees to see if everything in small tree matches
+	
+	public static boolean isSbBetter(TreeNode n1, TreeNode n2){
+		if(n1 == null && n2 == null) return true;
+		if(n2 == null) return true;
+		if(n1 == null) return false;
+//		todo: see if above can be merged into other condition
+		if(n1.data == n2.data && treeMatches(n1, n2)){
+			return true;
+		}else return isSbBetter(n1.left,n2) || isSbBetter(n1.right,n2);
+//		O(mn)time: every node in big tree check match small tree
+//		better bound: O(n + km) k:small tree's root occurance in big tree's
+//		O(lgn+lgm)space, better than O(m+n) -> scalability
+	}
+//	tree matches must match every node in exact same positions
+	public static boolean treeMatches(TreeNode n1, TreeNode n2){
+		if(n1 == null && n2 == null)		return true;
+		if(n1 == null || n2 == null)		return false;
+		if(n1.data != n2.data)		return false;
+		else		return treeMatches(n1.left,n2.left)&&treeMatches(n1.right,n2.right);
+	}
+//	O(m) time and O(lgm) space 
 
 	public static void main(String[] args) {
 		int[] array1 = {1, 2, 1, 3, 1, 1, 5};
-		int[] array2 = {1, 2, 3};
+		int[] array2 = {2, 3, 1};
 		TreeNode n1 = TreeNode.insertFromArray(array1);
 		TreeNode n2 = TreeNode.insertFromArray(array2);
-		if (isSbtree(n1, n2)) {
+		if (isSbBetter(n1, n2)) {
 			System.out.println("t2 is a subtree of t1");
 		} else {
 			System.out.println("t2 is not a subtree of t1");
